24c9f8a50d8ebd43e43dcec14cfa6fa5e372bcc4
Yandon29 <63966466+Yandon29@users.noreply.github.com>
2024-09-12 16:39:20 +0200

    Update __init__.py

--- __init__.py
+++ __init__.py
@@ -1,40 +1,22 @@
-from flask import Flask, render_template_string, render_template, jsonify
-from flask import render_template
-from flask import json
-from datetime import datetime
+from flask import Flask, jsonify, render_template
+import json
 from urllib.request import urlopen
-import sqlite3
-                                                                                                                                       
-app = Flask(__name__)                                                                                                                  
-                                                                                                                                       
+
+app = Flask(__name__)
+
+# Route pour la page d'accueil
 @app.route('/')
 def hello_world():
-    return render_template('hello.html') #comm222
+    return render_template('hello.html')  # comm222
 
+# Route pour la page de contact
 @app.route("/contact/")
 def MaPremiereAPI():
     return "<h2>Ma page de contact</h2>"
 
-
+# Route pour la météo de Tawarano (données brutes en JSON)
 @app.route('/tawarano/')
-def meteo():
-    response = urlopen('https://samples.openweathermap.org/data/2.5/forecast?lat=0&lon=0&appid=xxx')
-    raw_content = response.read()
-    json_content = json.loads(raw_content.decode('utf-8'))
-    results = []
-    for list_element in json_content.get('list', []):
-        dt_value = list_element.get('dt')
-        temp_day_value = list_element.get('main', {}).get('temp') - 273.15 # Conversion de Kelvin en °c 
-        results.append({'Jour': dt_value, 'temp': temp_day_value})
-    return jsonify(results=results)
-
-@app.route("/rapport/")
-def mongraphique():
-    return render_template("graphique.html")
-
-# Route pour afficher les données météo en format JSON
-@app.route('/meteo/')
-def meteo():
+def meteo_tawarano():
     response = urlopen('https://samples.openweathermap.org/data/2.5/forecast?lat=0&lon=0&appid=xxx')
     raw_content = response.read()
     json_content = json.loads(raw_content.decode('utf-8'))
@@ -46,6 +28,11 @@ def meteo():
         results.append({'Jour': dt_value, 'Température (°C)': temp_day_value})
     
     return jsonify(results=results)
+
+# Route pour afficher le graphique
+@app.route("/rapport/")
+def mongraphique():
+    return render_template("graphique.html")
 
 # Route pour afficher l'histogramme des températures
 @app.route('/histogramme/')
@@ -63,6 +50,6 @@ def histogramme():
 
     # Appel du template HTML pour afficher le graphique
     return render_template('histogramme.html', data=json.dumps(results))
-  
-if __name__ == "__main__":
-  app.run(debug=True)
+
+if __name__ == '__main__':
+    app.run(debug=True)
